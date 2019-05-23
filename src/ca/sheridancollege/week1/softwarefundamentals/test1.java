bce88c70f3cc704eb3c820289c25968c090a602d
troya <troya@LAPTOP-5PMIODHU>
2019-05-23 13:12:17 -0400

    commit

--- src/ca/sheridancollege/week1/softwarefundamentals/test1.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/test1.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Parttime;
+package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
  *
  * @author troya
  */
-public class java
+public class test1 {
     
 }
